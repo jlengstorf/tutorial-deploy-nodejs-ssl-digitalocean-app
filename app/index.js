2abca5b9416564f82edaa7a9402d72f71ea114f1
jlengstorf <jason.lengstorf@copterlabs.com>
2016-07-30 14:06:43 +0200

    style(app): comments and formatting cleanup

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// We're setting up an extremely simple server here.
+// We’re setting up an extremely simple server here.
 const http = require('http');
 
 // These could (should) be set as env vars.
@@ -9,8 +9,15 @@ const host = process.env.HOST || 'localhost';
 
 // No matter what hits the server, we send the same thing.
 http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+
+  // Tell the browser what’s coming.
+  res.writeHead(200, {
+    'Content-Type': 'text/html; charset=utf-8',
+  });
+
+  // Send a simple message in HTML.
   res.end('<h1>I’m a Node app!</h1><p>I’m secure!</p>');
 }).listen(port, host);
 
+// This message prints in the console when the app starts.
 console.log(`App running at http://${host}:${port}`);
